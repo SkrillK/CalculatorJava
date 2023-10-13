9166628aadaf91ad7283c0ccc89d98df0f3a17e4
unknown <leonid15072004@gmail.com>
2023-10-13 23:30:17 +0300

    v4

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,85 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double num1 = 0, num2 = 0, result = 0;
-        String operation = " ";
-        boolean continue_calculating = false, stop = false;
+    public static class Calculation {
+        private static Scanner input = new Scanner(System.in);
+        private static double number1, number2, result;
+        private static String operation;
+        boolean stop;
 
-        while(!stop)
-        {
-            if (continue_calculating)
-            {
-                num1 = result;
-                System.out.println("\nnum1 = " + num1);
-            }
-            else
-            {
-                System.out.printf("num1 = ");
-                num1 = input.nextDouble();
-                continue_calculating = true;
-            }
+        Calculation() {
+            this.number1 = 0;
+            this.number2 = 0;
+            this.operation = "";
+            this.stop = false;
+        }
 
-            System.out.printf("num2 = ");
-            num2 = input.nextDouble();
-
-            System.out.printf("operation: ");
-            operation = input.next();
-            switch (operation)
-            {
+        public void Run() {
+            switch (this.operation) {
                 case "+":
-                    System.out.println("num1 + num2 = " + (result = num1 + num2));
+                    this.result = this.number1 + this.number2;
                     break;
                 case "-":
-                    System.out.println("num1 - num2 = " + (result = num1 - num2));
+                    this.result = this.number1 - this.number2;
                     break;
                 case "*":
-                    System.out.println("num1 * num2 = " + (result = num1 * num2));
+                    this.result = this.number1 * this.number2;
                     break;
                 case "/":
-                    System.out.println("num1 / num2 = " + (result = num1 / num2));
+                    this.result = this.number1 / this.number2;
+                    break;
+                case "s":
+                    this.stop = true;
                     break;
                 default:
-                    stop = true;
+                    System.out.println("Program can't read this operation...\n" +
+                            "Try again or enter 's' to stop the program");
+                    get_operation();
                     break;
             }
+        }
+
+        public void get_input() {
+            System.out.printf("num1 = ");
+            this.number1 = input.nextDouble();
+            System.out.printf("num2 = ");
+            this.number2 = input.nextDouble();
+        }
+
+        public void get_input(double last_result) {
+            System.out.println("\nlast result = " + (this.number1 = last_result));
+            System.out.printf("num2 = ");
+            this.number2 = input.nextDouble();
+        }
+
+        public void get_operation() {
+            System.out.printf("operation: ");
+            this.operation = input.next();
+            Run();
+        }
+
+        public double return_result() {
+            return this.result;
+        }
+    }
+
+    public static void main(String[] args) {
+        Calculation calc = new Calculation();
+        boolean next = false;
+        double result = 0;
+
+        while (true) {
+            if (!next)
+                calc.get_input();
+            else
+                calc.get_input(result);
+
+            calc.get_operation();
+            if(calc.stop)
+                break;
+
+            System.out.println("result = " + (result = calc.return_result()));
+            next = true;
         }
     }
 }
