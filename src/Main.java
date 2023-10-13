37c00dea768aaaa8470a79e8733eaca1771c6a13
unknown <leonid15072004@gmail.com>
2023-10-13 21:26:24 +0300

    v1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 25, y = 10;
+        System.out.println("x = 25");
+        System.out.println("x = 25");
+
+        System.out.println("x + y = " + (x + y));
+        System.out.println("x - y = " + (x - y));
+        System.out.println("x * y = " + (x * y));
+        System.out.println("x / y = " + (x / y));
+    }
+}
